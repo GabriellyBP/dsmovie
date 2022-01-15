839d8dcf5f1743b6af8825a42c590a6c786736cd
GabriellyBP <bihpisaneschi@gmail.com>
2022-01-15 00:00:23 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,18 +1,27 @@
-import Pagination from "components/Pagination";
-import MovieCard from "components/MovieCard";
-import axios from "axios";
-import { BASE_URL } from "utils/request";
+ import Pagination from "components/Pagination";
+ import MovieCard from "components/MovieCard";
+ import axios from "axios";
+ import { BASE_URL } from "utils/request";
+ import { useState, useEffect } from "react";
+ import { MoviePage} from "types/movie";
 
-function Listing() {
 
-//FORMA ERRADA
-axios.get(`${BASE_URL}/movies?size=12&page=0`)
-.then(response => {
-    console.log(response.data);
-   }); 
+ function Listing() {
+
+    const [pageNumber, setPageNumber] = useState(0); 
+
+       useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
+        .then(response => {
+             const data = response.data as MoviePage;
+             console.log(data);
+           setPageNumber(data.number);
+         }); 
+       }, []);
 
     return (
         <>
+         <p>{pageNumber}</p> 
             <Pagination />
 
             <div className="container">
