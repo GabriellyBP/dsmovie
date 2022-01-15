2596446f1d851446eaf36a6330fff95334fa0012
GabriellyBP <bihpisaneschi@gmail.com>
2022-01-15 02:50:15 -0300

    Show score

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -24,10 +24,11 @@
 
     
        useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}`)
         .then(response => {
              const data = response.data as MoviePage;
-             setPage(data); 
+             setPage(data);
+
          }); 
        }, [pageNumber]);
 
@@ -38,20 +39,14 @@
             <div className="container">
                 <div className="row">
                     {page.content.map(movie => (
-                            <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard movie={movie} />
-                    </div>
+                        <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                            <MovieCard movie={movie} />
+                        </div>
                     )
-                    )}
-
-
-
-                             
-                  
+                    )}        
                 </div>
             </div>
         </>
-
     );
 }
 
